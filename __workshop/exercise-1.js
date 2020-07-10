64d41cefb6ab6116c14aed30f17a76829478782d
“Marc <marc_piantone@live.fr>
2020-07-10 15:51:41 -0400

    Slowly start

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,14 +1,40 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  capsPromise = new Promise((resolve, reject) => {
+    if (
+      array.every((element) => {
+        return typeof element === "string";
+      })
+    ) {
+      const newArray = array.map((element) => {
+        return element.toUpperCase();
+      });
+      resolve(newArray);
+    } else {
+      reject("The array is not made entirely of strings");
+    }
+  });
+  return capsPromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  sortPromise = new Promise((resolve, reject) => {
+    if (
+      array.every((element) => {
+        return typeof element === "string";
+      })
+    ) {
+      console.log(array.sort());
+      resolve(array.sort());
+    } else {
+      reject("The array is not made entirely of strings");
+    }
+  });
+  return sortPromise;
 };
 
 // Calling (testing)
