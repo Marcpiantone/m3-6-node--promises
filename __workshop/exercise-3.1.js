834dff21ce7c642fba2d71f0a1aaf7e7893a841a
“Marc <marc_piantone@live.fr>
2020-07-13 10:42:08 -0400

    Up to 3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,19 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
+const { get } = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json") //1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse.iss_position;
+    })
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
 };
+
+getIssPosition().then((result) => console.log(result));
--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,28 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+const request = require("request");
+require("dotenv").config();
+
+console.log(process.env.OPENCAGE_API_KEY);
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
   };
-
+  return request(
+    //`https://api.opencagedata.com/geocode/v1/json?q=${q}&key=${key}`
+    `https://api.opencagedata.com/geocode/v1/json?${requestObj}`
+  )
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse.results;
+    });
   // return something...
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
 ).then((response) => console.log(response));
